3feaaf3c2c103eb154fce9fe35ac2877276ed598
ShyShutaii <47605097+ShyShutaii@users.noreply.github.com>
2020-04-22 16:30:42 +0100

    Add files via upload

---
+++ Made Work/WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,68 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCallCalc
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.p = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Impact", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(230, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(281, 50)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "Call Calculator"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'p
+        '
+        Me.p.BackgroundImage = Global.WindowsApplication1.My.Resources.Resources.IMG02987363672224
+        Me.p.ForeColor = System.Drawing.Color.White
+        Me.p.Location = New System.Drawing.Point(-34, 272)
+        Me.p.Name = "p"
+        Me.p.Size = New System.Drawing.Size(812, 713)
+        Me.p.TabIndex = 1
+        Me.p.Text = "."
+        Me.p.UseVisualStyleBackColor = True
+        '
+        'frmCallCalc
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(769, 803)
+        Me.Controls.Add(Me.p)
+        Me.Controls.Add(Me.TextBox1)
+        Me.MaximumSize = New System.Drawing.Size(785, 842)
+        Me.MinimumSize = New System.Drawing.Size(785, 842)
+        Me.Name = "frmCallCalc"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents p As System.Windows.Forms.Button
+
+End Class
---
+++ Made Work/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class frmCallCalc
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles p.Click
+
+    End Sub
+End Class
